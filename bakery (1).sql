2a260b69ad6050a1d06b74107d8135b0f2026178
Mohamed El Mehdi Bougamouza <bougamouzamehdi@gmail.com>
2025-12-26 14:00:31 +0100

    Create orders table with necessary columns
    Created 'orders' table with relevant fields.

--- bakery (1).sql
+++ bakery (1).sql
@@ -2,3 +2,8 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 CREATE TABLE `orders` (
+`id` int(11) NOT NULL COMMENT 'chek it',
+  `Full_Name` varchar(100) NOT NULL,
+  `Phone` varchar(20) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Product_id` varchar(50) NOT NULL
