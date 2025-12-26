c46606dcf369af0197f6aec9adc69f39ed0ef905
Mohamed El Mehdi Bougamouza <bougamouzamehdi@gmail.com>
2025-12-26 22:29:34 +0100

    Add banner section with title and description

--- frontend.php.php
+++ frontend.php.php
@@ -7,3 +7,10 @@
 </head>
 <body class="Home-body">
    <?php include 'header.html';?>
+<section class="banner">
+    <h1>Berry & Bloom</h1><br>
+    <p>Fresh, Lovely, and Made with Love </p>
+    <div class="social-icons"><section class="banner">
+    <h1>Berry & Bloom</h1><br>
+    <p>Fresh, Lovely, and Made with Love </p>
+    <div class="social-icons">
