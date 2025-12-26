30aca16372f61aa8d2a7bb3e2f95b439cf082f6e
Mohamed El Mehdi Bougamouza <bougamouzamehdi@gmail.com>
2025-12-26 11:16:50 +0100

    first commit

---
+++ bakery (1).sql
@@ -0,0 +1,9 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 04, 2025 at 08:18 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
