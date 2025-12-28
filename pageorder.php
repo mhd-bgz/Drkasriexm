9faa23a5c238bf2014a8014338bdf9e7481ef6f3
abderrahmanebechiri <abderrahmane.b.info@gmail.com>
2025-12-28 01:22:46 +0100

    Add database connection to pageorder.php

--- pageorder.php
+++ pageorder.php
@@ -4,3 +4,9 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "bakery";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn-> connect_error ) {
+    die("error" . $conn-> connect_error );
+}
