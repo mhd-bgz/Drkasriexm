84add3bb2d566f327b2f32151cf4a9b520b56c48
abderrahmanebechiri <abderrahmane.b.info@gmail.com>
2025-12-28 15:47:53 +0100

    Update pageorder.php

--- pageorder.php
+++ pageorder.php
@@ -10,3 +10,11 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn-> connect_error ) {
     die("error" . $conn-> connect_error );
 }
+
+// save bdd
+$message = "";
+if (isset($_POST['submit'])) {
+    $full_name = $_POST['full_name'];
+    $phone = $_POST['phone'];
+    $email = $_POST['email'];
+    $product_id = $_POST['product_id'];
