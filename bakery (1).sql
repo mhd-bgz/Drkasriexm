1edeb4dbdb282f9723f8c1ff2a3b35b8688c7c3b
Mohamed El Mehdi Bougamouza <bougamouzamehdi@gmail.com>
2025-12-28 12:37:37 +0100

    Modify orders table structure and insert data
    Added primary key to orders table and inserted a sample order.

--- bakery (1).sql
+++ bakery (1).sql
@@ -7,3 +7,9 @@ CREATE TABLE `orders` (
   `Phone` varchar(20) NOT NULL,
   `Email` varchar(100) NOT NULL,
   `Product_id` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `orders` (`id`, `Full_Name`, `Phone`, `Email`, `Product_id`) VALUES
+(0, 'boukradoua', '0658254234', 'douaaboukraa2003@gmail.com', 'S-CHEESE');
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
