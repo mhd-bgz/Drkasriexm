9de164c8afcdf21dbabb7254a50258170e59b69f
Mohamed El Mehdi Bougamouza <bougamouzamehdi@gmail.com>
2025-12-30 13:29:43 +0100

    Add SQL script to create orders table

--- bdd/bakery.sql
+++ bdd/bakery.sql
@@ -1,5 +1,15 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
-
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+CREATE TABLE `orders` (
+`id` int(11) NOT NULL COMMENT 'chek it',
+  `Full_Name` varchar(100) NOT NULL,
+  `Phone` varchar(20) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Product_id` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `orders` (`id`, `Full_Name`, `Phone`, `Email`, `Product_id`) VALUES
+(0, 'boukradoua', '0658254234', 'douaaboukraa2003@gmail.com', 'S-CHEESE');
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
